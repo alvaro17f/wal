e47f29e00d85f78fcb6d112d16a236552adf3276
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-07-07 18:33:30 +0200

    chore(flake): update hash

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
 
           app = builtins.fetchTarball {
             url = "https://github.com/alvaro17f/${name}/releases/latest/download/owa-${system}.tar.gz";
-            sha256 = "sha256:1njjg9h68wz9j79n3mgimmrr83q4zl4vap029l1rmdi74ma7jz81";
+            sha256 = "sha256:11adf6d29x0v0jd2nl3av87lc146nrbdkvp0kc4wc77yzrph1439";
           };
 
           src = ./.;
